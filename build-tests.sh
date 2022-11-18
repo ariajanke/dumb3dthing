608637c8b482c7d8dd2a48b4a55c33c79dc8b0fe
ariajanke <ajanke@umich.edu>
2022-11-18 18:56:43 -0500

    CI attempt no. 1

---
+++ build-tests.sh
@@ -0,0 +1,7 @@
+g++ -O3 -std=c++17 \
+  $(find src/platform/test | grep 'cpp\b') $(find src -maxdepth 2 | grep 'cpp\b') \
+  lib/tinyxml2/tinyxml2.cpp \
+  -Ilib/cul/inc -Ilib/ecs3/inc -Ilib/tinyxml2 \
+  -Wno-unqualified-std-cast-call \
+  -o bin/.out-unit-tests
+./bin/.out-unit-tests
